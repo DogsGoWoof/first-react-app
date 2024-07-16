a330323d5292eae21b6a4fd5ebc34ed3931ffe46
jason <jason_tan75@hotmail.com>
2024-07-16 13:38:14 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+// src/App.jsx
+
+// const App = () => {
+
+//   return (
+//     <h1>Hello, world!</h1>
+//   );
+// };
+
+// export default App;
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <h1>Second</h1>
+      <ul>
+        <li>first item</li>
+      </ul>
+      <hr />
+    </>
+  );
+};
+
+export default App;
